95c33974f1a3faecf810552dae9a7a84bbb1485c
ValcocerLopez <vmalcocer@uamv.edu.ni>
2026-06-17 03:41:14 -0600

    Fix: Corregir orden de imports en usuarioRoutes

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
-
-router.get('/profesores', verificarToken, verificarRol(['admin']), usuarioController.obtenerProfesores);
 const { verificarToken, verificarRol } = require('../middlewares/authMiddleware');
 
-// Ver todos los usuarios (Solo Admin)
+router.get('/profesores', verificarToken, verificarRol(['admin']), usuarioController.obtenerProfesores);
 router.get('/', verificarToken, verificarRol(['admin']), usuarioController.obtenerUsuarios);
-
-// Actualizar un usuario por su ID (Solo Admin)
 router.put('/:id', verificarToken, verificarRol(['admin']), usuarioController.actualizarUsuario);
-
-// Eliminar un usuario por su ID (Solo Admin)
 router.delete('/:id', verificarToken, verificarRol(['admin']), usuarioController.eliminarUsuario);
 
 module.exports = router;
